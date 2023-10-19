ba7a9cd2cb677066bc790d0e1f20987281c1fa5e
IsaT <rakiib010@gmail.com>
2023-10-19 15:06:53 +0600

    cart collections create and get cart data

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kfd97zi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,15 +28,46 @@ async function run() {
       .db("digitalNexusDB")
       .collection("products");
     const brandCollection = client.db("digitalNexusDB").collection("brands");
+    const cartCollection = client.db("digitalNexusDB").collection("cart");
 
     app.get("/brands", async (req, res) => {
       const result = await brandCollection.find().toArray();
       res.send(result);
     });
 
+    app.get("/products", async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/cart", async (req, res) => {
+      const result = await cartCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/products/:brand", async (req, res) => {
+      const { brand } = req.params;
+      const query = { brandName: brand };
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/product/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/products", async (req, res) => {
       const product = req.body;
       const result = await productCollection.insertOne(product);
+      res.send(result);
+    });
+
+    app.post("/cart", async (req, res) => {
+      const product = req.body;
+      const result = await cartCollection.insertOne(product);
       res.send(result);
     });
     // Send a ping to confirm a successful connection
